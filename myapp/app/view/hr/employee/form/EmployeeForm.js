fae97e338964e4b001d0981034bd85fbe2111077
Muhyung Park <muhyung@miraeweb.com>
2023-06-14 16:09:52 +0900

    1.09 Form & Fields

--- myapp/app/view/hr/employee/form/EmployeeForm.js
+++ myapp/app/view/hr/employee/form/EmployeeForm.js
@@ -1,13 +1,95 @@
 
 Ext.define('MyApp.view.hr.employee.form.EmployeeForm',{
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.form.Panel',
     alias : 'widget.employee-form',
     requires: [
+        'Ext.button.Button',
+        'Ext.data.Store',
+        'Ext.form.FieldContainer',
+        'Ext.form.field.ComboBox',
+        'Ext.form.field.Date',
+        'Ext.form.field.Radio',
+        'Ext.form.field.Text',
+        'Ext.layout.container.HBox',
+        'Ext.toolbar.Fill',
         'MyApp.view.hr.employee.form.EmployeeFormController'
     ],
 
     controller: 'employee-form',
+    tbar :[{
+        xtype : 'tbfill'
+    },{
+        xtype : 'button',
+        text : 'Save'
+    }],
+    layout :'column',
+    bodyPadding : '10 10 10 20',
+    defaults : {
+        columnWidth : .333,
+        margin : '0 10 10 0',
 
+    },
 
-    html: 'Hello, World!!'
+    items : [{
+        xtype : 'textfield',
+        allowBlank : false,
+        blankText : 'ID is required',
+        fieldLabel : 'ID'
+    },{
+        xtype : 'textfield',
+        fieldLabel : 'Name'
+    },{
+        xtype : 'fieldcontainer',
+        fieldLabel : 'Gender',
+        layout : {
+            type : 'hbox',
+            align : 'stretch'
+        },
+        items : [{
+            xtype : 'radio',
+            flex : 1,
+            name : 'genderCode',
+            boxLabel : 'Male',
+            inputValue : 'MALE'
+        },{
+            xtype : 'radio',
+            flex : 1,
+            name : 'genderCode',
+            boxLabel : 'Female',
+            inputValue : 'FEMALE'
+        }]
+    },{
+        xtype : 'datefield',
+        format : 'Y.m.d',
+        altFormats: 'Y.m.d|Ymd|Y-m-d',
+        submitFormat : 'Ymd',
+        fieldLabel : 'BirthDate'
+    },{
+        xtype : 'textfield',
+        fieldLabel : 'Mobile'
+    },{
+        xtype : 'textfield',
+        fieldLabel : 'Email',
+        vtype : 'email'
+    },{
+        xtype : 'combobox',
+        fieldLabel : 'Dept.',
+        store : Ext.create('Ext.data.Store',{
+           data : [
+               {code : 'Dept', codeName : 'Dept.'}
+           ]
+        }),
+        displayField: 'codeName',
+        valueField : 'code'
+    },{
+        xtype : 'combobox',
+        fieldLabel : 'Rank',
+        store : Ext.create('Ext.data.Store',{
+            data : [
+                {code : 'Rank', codeName : 'Rank'}
+            ]
+        }),
+        displayField: 'codeName',
+        valueField : 'code'
+    }]
 });
