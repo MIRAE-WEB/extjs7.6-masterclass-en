ba1250eaa0af4399ba4cc264f7eef4f623778a2d
Muhyung Park <muhyung@miraeweb.com>
2023-06-16 18:41:58 +0900

    1.17 MVC Architecture & URI Router

--- myapp/app/view/global/center/tab/GlobalCenterTab.js
+++ myapp/app/view/global/center/tab/GlobalCenterTab.js
@@ -1,16 +1,5 @@
 
 Ext.define('MyApp.view.global.center.tab.GlobalCenterTab',{
     extend: 'Ext.tab.Panel',
-    alias : 'widget.global-center-tab',
-
-    items :[{
-        xtype : 'employee-management',
-        title : 'Employees'
-    },{
-        xtype : 'menu-management',
-        title : 'Menu'
-    },{
-        xtype : 'code-management',
-        title : 'Code'
-    }]
+    alias : 'widget.global-center-tab'
 });
